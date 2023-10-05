11439413e12d58aed30c494756e9e256f80b1a1e
hiSpace0 <2103941820@qq.com>
2023-10-05 10:43:34 +0800

    1005 commit

--- README.md
+++ README.md
@@ -5,8 +5,18 @@ https://spring.io/guides/gs/serving-web-content/
 
 ### git github代码托管
 git init <br>
-git add README.md <br>
-git commit -m "first commit"<br>
-git branch -M main<br>
-git remote add origin https://github.com/hiSpace0/community.git <br>
+git add README.md 代码改后要写入暂存区<br>
+git commit -m "first commit" 添加到本地仓库<br>
+git branch -M main 本地分支<br>
+git remote add origin https://github.com/hiSpace0/community.git 远程主机名<br>
 git push -u origin main
+
+### 快捷键
+ctrl + shift f12 代码区大屏
+ctrl + shift n 查找文件
+
+### 数据库配置
+Lombok <br>
+MyBatis Framework <br>
+MySQL Driver <br>
+通用Mapper4 增删改查 <br>
--- src/main/java/bbs/example/community/Controller/HelloController.java
+++ src/main/java/bbs/example/community/Controller/HelloController.java
@@ -1,16 +1,24 @@
 package bbs.example.community.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/")
+    public String index() {return "index";}
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
     }
+
+    @RequestMapping("/register")
+    public String register(){
+        return "register";
+    }
+
 }
+
